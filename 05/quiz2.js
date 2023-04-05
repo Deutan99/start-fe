e8fe3611fd4e46830ea08982ea5851bd60934169
HEO JAE HYEOK <41223338+Deutan99@users.noreply.github.com>
2023-04-06 00:00:35 +0900

    start-fe

--- 05/quiz2.js
+++ 05/quiz2.js
@@ -1,7 +1,46 @@
-var board = document.getElementById('board');
+var $wrap = document.querySelector('.wrap');
+var str = '';
+var className = '';
+var isWhite = false;
+var selectedDom = null;
+
+// 마크업을 코드로 찍어주기
 for (var i = 0; i < 4; i++) {
   for (var j = 0; j < 4; j++) {
-    // TODO : 여기에 필요한 코드를 작성해주세요.
-    let black = document.createElement('div');
+    if (isWhite) {
+      className = 'white';
+    } else {
+      className = 'black';
+    }
+
+    str += '<div class="' + className + '"></div>';
+
+    //한번씩 변경
+    isWhite = !isWhite;
   }
+
+  isWhite = !isWhite;
+}
+
+$wrap.innerHTML = str;
+
+// DOM에 클릭 이벤트 할당
+
+var boards = document.querySelectorAll('.wrap > div');
+
+function selectBoard(event) {
+  console.log('selected');
+  var board = event.currentTarget;
+  // console.log(board);
+
+  if (selectedDom) {
+    selectedDom.className = selectedDom.className.replace(' select', '');
+  }
+
+  board.className += ' select';
+  selectedDom = board;
+}
+
+for (var i = 0; i < boards.length; i++) {
+  boards[i].addEventListener('click', selectBoard);
 }
